9b00755dfe6941870a9c7feb97c450b0f5f153a9
Le Hoang <seiji@o2.pl>
2020-05-25 19:37:16 +0100

    update json

--- dist/utils/database.js
+++ dist/utils/database.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 // 'database', 'username', 'password'
-exports.sequelize = new sequelize_1.Sequelize("usanails", process.env.DB_USER, process.env.DB_PASS, {
+exports.sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: "postgres",
 });
