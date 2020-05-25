535199fc47846094886b1016d86fb9454ee473f7
Le Hoang <seiji@o2.pl>
2020-05-25 16:01:21 +0100

    database environment update

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 // 'database', 'username', 'password'
 
 export const sequelize = new Sequelize(
-  "usanails",
+  process.env.DB_NAME as string,
   process.env.DB_USER as string,
   process.env.DB_PASS as string,
   {
