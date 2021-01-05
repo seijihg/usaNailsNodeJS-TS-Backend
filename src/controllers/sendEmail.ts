4abe982aac11ffa9cc42271dbe34dccbe8863c46
Le H Ngo <seiji@o2.pl>
2021-01-05 19:01:43 +0000

    email nodemailer fixing bug

--- src/controllers/sendEmail.ts
+++ src/controllers/sendEmail.ts
@@ -15,12 +15,18 @@ export const contactEmail = (
   //   res.status(400).json({ error: error.message });
   // }
 
-  transporter.sendMail({
-    from: '"USA Nails - Berkhamsted" <no-reply@usa-nails.co.uk>', // sender address
-    to: "contact@usa-nails.co.uk", // list of receivers
-    subject: `Contact form USA Nails - Sender is ${req.body.name}`, // Subject line
-    text: `From ${req.body.name} and the email: ${req.body.email}. Message: ${req.body.content}`,
-  });
+  transporter.sendMail(
+    {
+      from: '"USA Nails - Berkhamsted" <no-reply@usa-nails.co.uk>', // sender address
+      to: "contact@usa-nails.co.uk", // list of receivers
+      subject: `Contact form USA Nails - Sender is ${req.body.name}`, // Subject line
+      text: `From ${req.body.name} and the email: ${req.body.email}. Message: ${req.body.content}`,
+    },
+    (err, info) => {
+      console.log(err);
+      console.log(info);
+    }
+  );
 
   res.json("Mail sent.");
 };
